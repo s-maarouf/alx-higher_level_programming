e9c204a4bcffe0fb950195efa2c2f446a11e7dbc
s-maarouf <maarouf.salahedd@gmail.com>
2023-05-09 10:34:35 -0700

    completing task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ascii_value in range(97, 122):
+for ascii_value in range(97, 123):
     char = chr(ascii_value)
-    print("{}".format(char),end='')
+    print("{}".format(char), end='')
