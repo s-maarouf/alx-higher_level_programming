1f487865acf5982db878823792b55c9083d2db6f
Salah Eddine Maarouf <maarouf.salahedd@gmail.com>
2023-05-11 19:10:35 +0100

    completing task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,12 @@
 if __name__ == "__main__":
 
     import sys
-count = len(sys.argv)
+    count = len(sys.argv) - 1
 if count == 0:
-	
+    print("{} arguments.".format(count))
+elif count == 1:
+    print("{} argument: {}".format(count, sys.argv[+1]))
+else:
+    print("{} arguments:".format(count,))
+for i in range(count):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
