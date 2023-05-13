1bde82e9a12b654cffa3210b2d4cec1d76080a2c
s-maarouf <maarouf.salahedd@gmail.com>
2023-05-13 07:54:33 -0700

    completing task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    if 'c' in my_string or 'C' in my_string:
-        my_string = ''.join([c for c in my_string if c.lower() != 'c'])
-        my_string = ''.join([C for C in my_string if C != 'C'])
-        return my_string
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
