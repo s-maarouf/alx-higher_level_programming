2001f6193a63c03475379f0219817813035747d8
s-maarouf <maarouf.salahedd@gmail.com>
2023-05-17 15:12:58 -0700

    completing task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+
+    roman_values = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    result = 0
+    prev_value = 0
+
+    for i in range(len(roman_string) - 1, -1, -1):
+        current_value = roman_values.get(roman_string[i], 0)
+
+        if current_value >= prev_value:
+            result += current_value
+        else:
+            result -= current_value
+
+        prev_value = current_value
+
+    return result
