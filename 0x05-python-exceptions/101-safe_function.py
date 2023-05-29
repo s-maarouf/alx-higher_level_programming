590ab5608e3f42bfabe83b18c3d4d3326179a6b5
s-maarouf <maarouf.salahedd@gmail.com>
2023-05-29 14:10:00 -0700

    Completing task 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+
+    try:
+        res = fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
+    else:
+        return res
