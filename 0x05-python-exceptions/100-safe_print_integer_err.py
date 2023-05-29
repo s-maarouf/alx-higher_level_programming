be65a0d211fcb74fd675e4e9911dca0115d92441
s-maarouf <maarouf.salahedd@gmail.com>
2023-05-29 14:05:45 -0700

    Completing task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -9,5 +9,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
-        print("Exception: {}".format(value), file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
