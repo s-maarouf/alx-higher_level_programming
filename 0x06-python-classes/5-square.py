e3533bfd6350cfe999125fc4943c97e7bd21ca84
s-maarouf <maarouf.salahedd@gmail.com>
2023-05-30 15:18:30 -0700

    Completing task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""define a square"""
+
+
+class Square:
+    """defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square
+
+        Args: size of square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns square area"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
