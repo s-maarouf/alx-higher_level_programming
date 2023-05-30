4e011bce7d5135b0fa5de67716e197a692a81ab6
s-maarouf <maarouf.salahedd@gmail.com>
2023-05-30 13:36:34 -0700

    Completing task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""define a square"""
+
+
+class Square:
+    """defines a square"""
+
+    def __init__(self, size):
+        """Initialize a new square
+
+        Args: size of square"""
+        self.__size = size
