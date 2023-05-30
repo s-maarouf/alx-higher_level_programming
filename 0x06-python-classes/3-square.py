641083f8440915034aaa5db3186df354dad9cdc7
s-maarouf <maarouf.salahedd@gmail.com>
2023-05-30 13:53:32 -0700

    Completing task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""define a square"""
+
+
+class Square:
+    """defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square
+
+        Args: size of square"""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns square area"""
+        return (self.__size * self.__size)
