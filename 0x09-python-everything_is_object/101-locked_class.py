9af1c6d23eafca330ac27ea80551bb935f0d081c
s-maarouf <maarouf.salahedd@gmail.com>
2023-06-06 15:40:48 -0700

    Completed task 28

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""Define locked class"""
+
+
 class LockedClass:
+
+    """prevents the user from creating new instance
+       attributes except if the new instance
+       attribute is called first_name"""
 
     __slots__ = ["first_name"]
