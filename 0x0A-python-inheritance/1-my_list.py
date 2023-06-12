445bd343a8246dfd0009e859fcc11d56c5e4325c
s-maarouf <maarouf.salahedd@gmail.com>
2023-06-12 15:53:57 -0700

    Completed task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,10 @@
 
 class MyList(list):
 
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
 
         """Prints sorted list"""
