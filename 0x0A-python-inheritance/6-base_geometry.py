64afc55eec481d40d6b6648431ecdd482fff35d5
s-maarouf <maarouf.salahedd@gmail.com>
2023-06-12 16:18:08 -0700

    Completed task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define BaseGeometry class"""
+
+
+class BaseGeometry:
+
+    """BaseGeometry class"""
+
+    def area(self):
+
+        """raises an Exception"""
+
+        raise Exception("area() is not implemented")
