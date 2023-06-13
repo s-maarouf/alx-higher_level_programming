ed6eb2fd291face8c59fa0ac710aa87dbeb6b57b
s-maarouf <maarouf.salahedd@gmail.com>
2023-06-13 15:11:12 -0700

    Fixing task 0 encoding requirement

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,6 @@ def read_file(filename=""):
 
     """function that reads a text file (UTF8) and prints it to stdout"""
 
-    with open(filename, 'r') as file:
+    with open(filename, encoding="utf-8") as file:
         content = file.read()
     print(content)
