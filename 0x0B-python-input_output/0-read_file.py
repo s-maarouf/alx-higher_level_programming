625e4a6d7c3da5c41f9becd0ba8c68dde390e5a3
s-maarouf <maarouf.salahedd@gmail.com>
2023-06-13 15:09:37 -0700

    Completed task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,6 @@ def read_file(filename=""):
 
     """function that reads a text file (UTF8) and prints it to stdout"""
 
-    file = open(filename, 'r')
-    content = file.read()
+    with open(filename, 'r') as file:
+        content = file.read()
     print(content)
