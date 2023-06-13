bf40b6e182a86fa8426330f8d9df27511db012f2
s-maarouf <maarouf.salahedd@gmail.com>
2023-06-13 15:26:52 -0700

    Completed task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Define JSON representation"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+
+    """function that returns the JSON representation of an object"""
+
+    return json.dumps(my_obj)
