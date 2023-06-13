7f119b72895795f752f46e71ee8c033c9c3bb24a
s-maarouf <maarouf.salahedd@gmail.com>
2023-06-13 16:35:24 -0700

    Completed task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Define class to JSON"""
+
+
+class Student:
+
+    """defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+
+        """Initialize a new Student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+
+        """retrieves a dictionary representation of a Student"""
+
+        return self.__dict__
