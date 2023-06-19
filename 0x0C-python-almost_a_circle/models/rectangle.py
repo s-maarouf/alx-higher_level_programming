79af2e25fa3ee93f7da381ab63c296cc02281bce
s-maarouf <maarouf.salahedd@gmail.com>
2023-06-19 15:30:24 -0700

    Completed task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,16 @@ class Rectangle(Base):
     def __str__(self):
         """Return string representation of Rectangle"""
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """assigns an argument to each attribute"""
+        if len(args) > 0:
+            self.__id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
