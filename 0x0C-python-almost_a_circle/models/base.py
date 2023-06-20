fecd2715bccaa3f212b619be721f0a5bf94e05ef
s-maarouf <maarouf.salahedd@gmail.com>
2023-06-19 18:02:15 -0700

    Completed task 14 and 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 """define base class"""
 
 
+import json
+
+
 class Base:
 
     """initialise base class"""
@@ -19,3 +22,25 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+
+        """returns the JSON string representation of list_dictionaries"""
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+
+        """writes the JSON string representation of list_objs to a file"""
+
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                file.write(Base.to_json_string(list_dicts))
