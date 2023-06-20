8f0b8abf000c7c0dcacd3d016fb57fb58e750aa2
s-maarouf <maarouf.salahedd@gmail.com>
2023-06-19 18:05:05 -0700

    Completed task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,3 +44,12 @@ class Base:
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 file.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+
+        """returns the list of the JSON string representation json_string"""
+
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
