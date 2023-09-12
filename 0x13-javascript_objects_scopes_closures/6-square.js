06acf74219d69ce6affd14cc988ef5e54345b063
s-maarouf <maarouf.salahedd@gmail.com>
2023-09-12 23:27:22 +0100

    Completed task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Squared = require('./5-square.js');
+
+class Square extends Squared {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Square;
