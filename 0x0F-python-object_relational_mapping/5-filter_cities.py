0697f44eb5f4d30c2bee97bb1b855672e98747d6
s-maarouf <maarouf.salahedd@gmail.com>
2023-09-15 22:29:40 +0100

    Completed task 5: fixed typo

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-"""Lists all cities from the database hbtn_0e_0_usa"""
+"""
+Lists all cities taken as argument from the database hbtn_0e_0_usa
+"""
 
 import MySQLdb
 import sys
@@ -9,8 +10,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id WHERE states.name LIKE BINARY '{}' ORDER BY cities.id""".format(sys.argv[4]))
+    cur.execute("""SELECT cities.name FROM cities INNER JOIN states
+                   ON states.id=cities.state_id WHERE states.name LIKE BINARY '{}'
+                   ORDER BY cities.id""".format(sys.argv[4]))
     rows = cur.fetchall()
     print(", ".join([row[0] for row in rows]))
     cur.close()
