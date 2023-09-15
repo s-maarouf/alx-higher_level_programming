0c2bdb83f624bb65f300e5de170c49441b84f113
s-maarouf <maarouf.salahedd@gmail.com>
2023-09-15 22:31:23 +0100

    Completed task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""
-Lists all cities taken as argument from the database hbtn_0e_0_usa
-"""
-
+"""  lists all cities taken as argument from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM cities INNER JOIN states
-                   ON states.id=cities.state_id WHERE states.name LIKE BINARY '{}'
-                   ORDER BY cities.id""".format(sys.argv[4]))
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
     rows = cur.fetchall()
-    print(", ".join([row[0] for row in rows]))
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
     cur.close()
     db.close()
