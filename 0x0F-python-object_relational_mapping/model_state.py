50839ab3e165a5869bb83540cbd7f1234d8ae1bc
s-maarouf <maarouf.salahedd@gmail.com>
2023-09-16 19:46:32 +0100

    Fixed pycodestyle error

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,5 +12,5 @@ class State(Base):
     """Class State"""
 
     __tablename__ = 'states'
-    id = Column(Integer, unique=True ,primary_key=True, nullable=False)
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
