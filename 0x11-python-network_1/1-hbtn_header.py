259da93cf4b9301f86b254f0c2515db11ea8ea0f
s-maarouf <maarouf.salahedd@gmail.com>
2023-10-01 19:48:02 +0100

    Completed task 1: fixed requirement error

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,9 +3,10 @@
     X-Request-Id variable found in the header of the response."""
 
 import urllib.request
+import sys
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen(sys.argv[1]) as response:
         resp = response.info()
         print(resp['X-Request-Id'])
