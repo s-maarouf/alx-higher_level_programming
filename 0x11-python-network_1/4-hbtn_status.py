fb2cba5ffa3d8ba0421ffb592547632bc8573ae5
s-maarouf <maarouf.salahedd@gmail.com>
2023-10-01 19:57:45 +0100

    Completed task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status"""
+
+import requests
+
+
+if __name__ == "__main__":
+    resp = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(resp.text)))
+    print("\t- content: {}".format(resp.text))
